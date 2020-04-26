caf7b16c6dc687ff637037d6ff95ec46ea9e059e
mahfuz380 <almahfuz380@gmail.com>
2020-04-26 11:24:19 +0600

    connection and path section done

---
+++ admin/includes/new_config.php
@@ -0,0 +1,11 @@
+<?php
+//Database connection
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'root'); 
+define('DB_PASS', ''); 
+define('DB_NAME', 'gallary_db');
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+if($connection){
+	echo  "ture";
+} 
+?>
