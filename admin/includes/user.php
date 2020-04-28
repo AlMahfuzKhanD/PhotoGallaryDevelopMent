ad2cfd4399a9ae101825ec4110f841dce0896de0
mahfuz380 <almahfuz380@gmail.com>
2020-04-28 14:47:40 +0600

    find query method created

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,16 +2,21 @@
 class User {
 
 	public static function findAllUsers(){
-		global $database; // making database object global
-		$reslutSet = $database->query("SELECT * FROM users"); // executing query method from database class
-		return $reslutSet; // returning result for execution
+		return  self::findThisQuery("SELECT * FROM users");
 	} //end findAllUser method
 
 	public static function findUsersById($userId){
-		global $database;
-		$resultSet = $database->query("SELECT * FROM users WHERE id =$userId LIMIT 1"); // executing query
+		$resultSet = self::findThisQuery("SELECT * FROM users WHERE id =$userId LIMIT 1");
 		$userFound = mysqli_fetch_array($resultSet); // fetching data and saving int in a variable
 		return $userFound; //sending data
 	} //end findUserById method
+
+	public static function findThisQuery($sql){ //this method for query
+		global $database; // making database object global
+		$resultSet = $database->query($sql); // executing query method from database class and catching query using $sql variable
+		return $resultSet; // returning resultSet
+	} //end findThisQuery
+
+
 } //end User class
 ?>
