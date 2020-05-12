b99438bcc40755c1c5335b3cbb35045bb080cba0
mahfuz380 <almahfuz380@gmail.com>
2020-05-12 13:04:05 +0600

    common method separeted to a class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,5 +1,5 @@
 <?php
-class User {
+class User extends DbObject {
 
 	protected static $dbTable = "users";
 	protected static $dbTableFields = array('userName', 'password', 'firstName', 'lastName');
@@ -9,31 +9,9 @@ class User {
 	public $lastName;
 	public $password;
 
-	public static function findAll(){
-		return  self::findThisQuery("SELECT * FROM " . self::$dbTable ."");
-	} //end findAllUser method
+	
 
-	public static function findById($userId){
-		$theResultArray = self::findThisQuery("SELECT * FROM " . self::$dbTable ." WHERE id =$userId LIMIT 1");
-
-		return !empty($theResultArray) ? array_shift($theResultArray) : false; //catching first item of the array or return false
-
-		
-	} //end findUserById method
-
-	public static function findThisQuery($sql){ //this method for query
-		global $database; // making database object global
-
-		$resultSet = $database->query($sql); // executing query method from database class and catching query using $sql variable
-
-		$theObjectArray = array(); // empty array for putting objects
-
-		while($row = mysqli_fetch_array($resultSet)){
-			$theObjectArray[] = self::instantiation($row);
-		}
-
-		return $theObjectArray; // returning array value
-	} //end findThisQuery
+	
 
 	public static function verifyUser($userName,$password){
 		global $database;
@@ -49,31 +27,7 @@ class User {
 		return !empty($theResultArray) ? array_shift($theResultArray) : false;
 	}
 
-	public static function instantiation($userRecord){
-		$theObject = new self;
-
-		//this is manual way of  fetching data
-		// $theObject->id = $user['id'];
-		//$theObject->userName = $user['userName'];
-		//$theObject->firstName = $user['firstName'];
-		//$theObject->lastName = $user['lastName'];
-
-		foreach ($userRecord as $theAttribute => $value) {
-			if($theObject->hasTheAttribute($theAttribute)){
-				$theObject->$theAttribute = $value;
-			}
-		}
-
-
-        return $theObject;
-	} // end instantiation
-
-	private  function hasTheAttribute($theAttribute){
-		$objectProperties = get_object_vars($this); // collecting oproerties
-        return array_key_exists($theAttribute, $objectProperties); //it will return key exists or not
-
-
-	} //end of hasTheAttribute
+	
 
 	protected function properties(){ //this function will pull out all the  properties of the class
 		foreach (self::$dbTableFields as $dbField) {
