b753c4036ae216e9fb04923a17f89b21d65229af
mahfuz380 <almahfuz380@gmail.com>
2020-05-14 16:48:32 +0600

    photo page fronend modified

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -2,11 +2,44 @@
 class Comment extends DbObject { 
 
 	protected static $dbTable = "comments";
-	protected static $dbTableFields = array('photoId', 'author', 'body');
+	protected static $dbTableFields = array('id', 'photoId', 'author', 'body');
 	public $id;
 	public $photoId;
 	public $author;
 	public $body;
+
+
+	public static function createComment($photoId, $author="John", $body=""){
+
+		if(!empty($photoId) && !empty($author && !empty($body)){
+
+			$comment = new Comment();
+
+			$comment->photoId = (int)$photoId;
+			$comment->author  = $author;
+			$comment->body    = $body;
+
+			return $comment;
+
+		} else{
+			return false;
+		}//end if else
+
+		public static function findTheComments($photoId=0){
+
+			global $database;
+
+			$sql = "SELECT * FROM " . self::$dbTable;
+			$sql .= " WHERE photoId = " $database->scapeString($photoId);
+			$sql .= " ORDER BY photoId ASC" $photoId;
+
+			return self::findByQuery($sql);
+
+		} // end findTheComments
+
+	} // end createComment
+
+
 	
 
 } //end Comment class
--- photo.php
+++ photo.php
@@ -1,3 +1,30 @@
+<?php
+require_once("admin/includes/init.php");
+
+if(empty($_GET['id'])){
+    redirect("index.php");
+}
+
+$photo = Photo::findById($_GET['id']);
+$photo->title;
+
+if(isset($_POST['submit'])){
+
+    $author = trim($_POST['author']);
+    $body = trim($_POST['body']);
+
+    $newComment = Comment::createComment($photo->id, $author,$body);
+    if($newComment && $newComment->save()){
+
+        redirect("photo.php?id={$photo->id}");
+    }
+    
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -101,14 +128,20 @@
 
                 <!-- Blog Comments -->
 
+                
+
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form role="form" method="post">
+
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <input type="text" name="author" class="form-control" placeholder="Your Name">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3" name="body" placeholder="Your Comments"></textarea>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
@@ -129,31 +162,9 @@
                     </div>
                 </div>
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
+
+
+                
 
             </div>
 
