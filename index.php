cabd87d31917b536c15491b7f367fd84316bb103
mahfuz380 <almahfuz380@gmail.com>
2020-05-16 10:51:15 +0600

    frontend gallary image displayd

--- index.php
+++ index.php
@@ -1,26 +1,50 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+
+$photos = Photo::findAll();
+
+?>
+
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
+                <div class="thumbnails row">
+
+                <?php foreach ($photos as $photo): ?>
+
+
+                    
+
+                        <div class="col-xs-6 col-md-3">
+
+                            <a class="thumbnail" href="">
+                                
+                                <img src="admin/<?php echo $photo->picturePath();?>" alt="">
+
+                            </a>
+                            
+                        </div> <!-- end col-xs-6 col-md-12
+                         -->
+                   
+
+
+                <?php endforeach; ?>
+            </div> <!-- end row -->
 
     
-            
-          
-         
-
-            </div>
+</div> <!-- col-md-12 -->
 
 
 
 
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+            <!-- <div class="col-md-4"> -->
 
             
-                 <?php include("includes/sidebar.php"); ?>
+                 <?php //include("includes/sidebar.php"); ?>
 
 
 
